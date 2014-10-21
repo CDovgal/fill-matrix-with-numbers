2b3e888541af020fe162efa3dd55b953b7d13c03
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-10-21 23:27:07 +0300

    allmost working top right

--- FiilMatrixWithNumbers/src/main.cpp
+++ FiilMatrixWithNumbers/src/main.cpp
@@ -8,92 +8,121 @@ using namespace std;
 
 void multipliedMatrix(int n)
 {
-  vector <vector<int>> multi(n, vector<int>(n));
+	vector <vector<int>> multi(n, vector<int>(n));
 
-  for (int i = 0; i < n; ++i)
-  {
-    for (int j = 0; j < n; ++j)
-    {
-      multi[i][j] = i*j;
-    }
-  }
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			multi[i][j] = i*j;
+		}
+	}
 
-  for (int i = 0; i < n; ++i)
-  {
-    for (int j = 0; j < n; ++j)
-    {
-      cout << multi[i][j];
-    }
-    cout << "\n";
-  }
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cout << multi[i][j] << "\t";
+		}
+		cout << "\n";
+	}
 }
 
 void summedMatrix(int n)
 {
-  vector <vector<int>> summ(n, vector<int>(n));
+	vector <vector<int>> summ(n, vector<int>(n));
 
-  for (int i = 0; i < n; ++i)
-  {
-    for (int j = 0; j < n; ++j)
-    {
-      summ[i][j] = i + j;
-    }
-  }
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			summ[i][j] = i + j;
+		}
+	}
 
-  for (int i = 0; i < n; ++i)
-  {
-    for (int j = 0; j < n; ++j)
-    {
-      cout << summ[i][j];
-    }
-    cout << "\n";
-  }
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cout << summ[i][j] << "\t";
+		}
+		cout << "\n";
+	}
 }
 
 void fillBottomLeft(int n)
 {
-  vector <vector<int>> bottom_filled(n, vector<int>(n));
-  int k = 1;
-  int iCol = 0;
-  int iRow = n - 1;
+	vector <vector<int>> bottom_filled(n, vector<int>(n));
+	int k = 1;
+	int iCol = 0;
+	int iRow = n - 1;
 
-  while (iCol < n && iRow >= 0)
-  {
-    int iRowStep = iRow;
-    int iColStep = iCol;
+	while (iCol < n && iRow >= 0)
+	{
+		int iRowStep = iRow;
+		int iColStep = iCol;
 
-    if (iCol < n - 1)
-      iColStep = iCol++;
-    else
-      iRowStep = iRow--;
+		if (iCol < n - 1)
+			iColStep = iCol++;
+		else
+			iRowStep = iRow--;
 
-    while (iColStep >= 0 && iRowStep >= 0)
-      bottom_filled[iRowStep--][iColStep--] = k++;
-  }
+		while (iColStep >= 0 && iRowStep >= 0)
+			bottom_filled[iRowStep--][iColStep--] = k++;
+	}
 
-  for (int i = 0; i < n; ++i)
-  {
-    for (int j = 0; j < n; ++j)
-    {
-      cout << bottom_filled[i][j];
-    }
-    cout << "\n";
-  }
-  
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cout << bottom_filled[i][j] << "\t";
+		}
+		cout << "\n";
+	}
+
 }
 
 void fillTopRight(int n)
 {
-  vector <vector<int>> top_filled(n, vector<int>(n));
+	vector <vector<int>> top_filled(n, vector<int>(n));
+
+  int k = 1;
+  int iCol = n - 1;
+  int iRow = 0;
+
+  while (iCol >= 0 && iRow < n)
+  {
+      int iRowStep = iRow;
+      int iColStep = iCol;
+
+      if (iRow < n - 1)
+          iRowStep = iRow++;
+      else
+          iColStep = iCol--;
+
+      while (iColStep >= 0 && iRowStep >= 0)
+          top_filled[iRowStep--][iColStep--] = k++;
+  }
+
+  for (int i = 0; i < n; ++i)
+  {
+      for (int j = 0; j < n; ++j)
+      {
+          cout << top_filled[i][j] << "\t";
+      }
+      cout << "\n";
+  }
 }
 
 
 int main()
 {
-  cout << "Multiplied matrix\n";
-  multipliedMatrix(2);
-  cout << "Summed matrix\n";
-  summedMatrix(2);
-  cout << "Filled bottom\n";
-  fillBottomLeft(4);
+	cout << "Multiplied matrix\n";
+	multipliedMatrix(4);
+	cout << "Summed matrix\n";
+	summedMatrix(4);
+	cout << "Filled bottom left\n";
+	fillBottomLeft(4);
+	cout << "Filled top right\n";
+	fillTopRight(4);
 }
