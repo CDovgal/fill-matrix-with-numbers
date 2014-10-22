fdd5246cf292daba50e4abafac4a669dcf6e0934
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-10-22 17:46:07 +0300

    refactored Fibonacci fill

--- FiilMatrixWithNumbers/src/main.cpp
+++ FiilMatrixWithNumbers/src/main.cpp
@@ -9,69 +9,71 @@ using namespace std;
 
 void fillMultipliedIndexes(int n) //filled by multiplication of indexes
 {
-	vector <vector<int>> multi(n, vector<int>(n));
+  if (n <= 1)
+    return;
+  vector <vector<int>> multi(n, vector<int>(n));
 
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			multi[i][j] = i*j;
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+    {
+      multi[i][j] = i*j;
       cout << multi[i][j] << "\t";
-		}
+    }
     cout << "\n";
-	}
+  }
 }
 
 void fillSummedIndexes(int n) // filled by sum of matrix cells indexes
 {
-	vector <vector<int>> summ(n, vector<int>(n));
+  vector <vector<int>> summ(n, vector<int>(n));
 
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			summ[i][j] = i + j;
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+    {
+      summ[i][j] = i + j;
       cout << summ[i][j] << "\t";
-		}
+    }
     cout << "\n";
-	}
+  }
 }
 
 void fillBottomLeft(int n) // filled by numbers in course from bottom left corbner to top right
 {
-	vector <vector<int>> bottom_filled(n, vector<int>(n));
-	int k = 1;
-	int iCol = 0;
-	int iRow = n - 1;
+  vector <vector<int>> bottom_filled(n, vector<int>(n));
+  int k = 1;
+  int iCol = 0;
+  int iRow = n - 1;
 
-	while (iCol < n && iRow >= 0)
-	{
-		int iRowStep = iRow;
-		int iColStep = iCol;
+  while (iCol < n && iRow >= 0)
+  {
+    int iRowStep = iRow;
+    int iColStep = iCol;
 
-		if (iCol < n - 1)
-			iColStep = iCol++;
-		else
-			iRowStep = iRow--;
+    if (iCol < n - 1)
+      iColStep = iCol++;
+    else
+      iRowStep = iRow--;
 
-		while (iColStep >= 0 && iRowStep >= 0)
-			bottom_filled[iRowStep--][iColStep--] = k++;
-	}
+    while (iColStep >= 0 && iRowStep >= 0)
+      bottom_filled[iRowStep--][iColStep--] = k++;
+  }
 
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			cout << bottom_filled[i][j] << "\t";
-		}
-		cout << "\n";
-	}
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+    {
+      cout << bottom_filled[i][j] << "\t";
+    }
+    cout << "\n";
+  }
 
 }
 
 void fillTopRight(int n) // filled by numbers in course from top right corner to left bottom
 {
-	vector <vector<int>> top_filled(n, vector<int>(n));
+  vector <vector<int>> top_filled(n, vector<int>(n));
 
   int k = 1;
   int iCol = n - 1;
@@ -79,44 +81,45 @@ void fillTopRight(int n) // filled by numbers in course from top right corner to
 
   while (iCol >= 0 && iRow < n)
   {
-      int iRowStep = iRow;
-      int iColStep = iCol;
+    int iRowStep = iRow;
+    int iColStep = iCol;
 
-      if (iCol > 0)
-          iColStep = iCol--;
-      else
-          iRowStep = iRow++;
+    if (iCol > 0)
+      iColStep = iCol--;
+    else
+      iRowStep = iRow++;
 
-      while (iColStep < n && iRowStep < n)
-          top_filled[iRowStep++][iColStep++] = k++;
+    while (iColStep < n && iRowStep < n)
+      top_filled[iRowStep++][iColStep++] = k++;
   }
 
   for (int i = 0; i < n; ++i)
   {
-      for (int j = 0; j < n; ++j)
-      {
-          cout << top_filled[i][j] << "\t";
-      }
-      cout << "\n";
+    for (int j = 0; j < n; ++j)
+    {
+      cout << top_filled[i][j] << "\t";
+    }
+    cout << "\n";
   }
 }
 
-vector<int> fib(int n)
+vector<int> generateFibonacci(int n)
 {
-  int c, first = 0, second = 1, next;
+  int first = 0, second = 1;
+
   vector<int> fibo;
-  for (c = 0; c < n; c++)
+
+  for (int c = 0; c < n; ++c)
   {
-    if (c <= 1)
-      next = c;
+    if (c <= 0)
+      fibo.push_back(first);
     else
     {
-      next = first + second;
+      fibo.push_back(first + second);
       first = second;
-      second = next;
+      second = fibo.at(c);
     }
-    fibo.push_back(next);
-  } 
+  }
   return fibo;
 }
 
@@ -130,14 +133,7 @@ void fillByFibonacciNumb(int n) //filled by Fibonacci number in memory location 
   {
     for (int j = 0; j < n; ++j)
     {
-      fibnumb_filled[i][j] = fib(n*n).at(k++);
-    }
-  }
-
-  for (int i = 0; i < n; ++i)
-  {
-    for (int j = 0; j < n; ++j)
-    {
+      fibnumb_filled[i][j] = generateFibonacci(n*n).at(k++);
       cout << fibnumb_filled[i][j] << "\t";
     }
     cout << "\n";
@@ -148,17 +144,16 @@ void fillByFibonacciNumb(int n) //filled by Fibonacci number in memory location 
 
 int main(int argc, char **argv)
 {
-
-	cout << "Multiplied matrix\n";
-	fillMultipliedIndexes(4);
-	cout << "Summed matrix\n";
-	fillSummedIndexes(4);
-	cout << "Filled bottom left\n";
-	fillBottomLeft(4);
-	cout << "Filled top right\n";
-	fillTopRight(4);
+  cout << "Multiplied matrix\n";
+  fillMultipliedIndexes(4);
+  cout << "Summed matrix\n";
+  fillSummedIndexes(4);
+  cout << "Filled bottom left\n";
+  fillBottomLeft(4);
+  cout << "Filled top right\n";
+  fillTopRight(4);
   cout << "Filled by Fibonacci numbers\n";
   fillByFibonacciNumb(4);
-  
+
   return 0;
 }
