6dfc66d3a1f9b474872559b002789f00c7fbeafe
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-10-27 17:39:28 +0200

    Ticket test

--- Ticket/src/main.cpp
+++ Ticket/src/main.cpp
@@ -2,9 +2,30 @@
 
 using namespace std;
 
+
+bool checkIfLucky(int number)
+{
+  while (number > 0)
+  {
+    int digit = number % 10;
+    number /= 10;
+    
+  }
+
+  return true;
+}
+
 int main()
 {
+  int number;
 
+  cout << "Please enter number of ticket: " << endl;
+
+  cin >> number;
+
+  if (checkIfLucky(number))
+    cout << "You are lucky today!!!\n";
+  cout << "Sorry, another time.. :(\n";
 
 
   return 0;
