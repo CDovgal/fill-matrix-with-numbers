d358bc7497f56416479b946093f9ee3274e1ed98
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-10-27 23:45:11 +0200

    lucky ticket check

--- Ticket/src/main.cpp
+++ Ticket/src/main.cpp
@@ -3,17 +3,36 @@
 using namespace std;
 
 
-bool checkIfLucky(int number)
+int digit_sum(int number)
 {
+  unsigned sum = 0;
+
   while (number > 0)
   {
     int digit = number % 10;
     number /= 10;
-    
+    sum+=digit;
   }
 
-  return true;
+  return sum;
 }
+
+
+bool checkIfLucky(int number)
+{
+  int sum_left = 0;
+  int sum_right = 0;
+  int left = number % 1000; 
+  int right = number / 1000;
+
+  if (digit_sum(left) == digit_sum(right))
+    return true;
+
+  return false;
+}
+
+
+
 
 int main()
 {
@@ -25,7 +44,8 @@ int main()
 
   if (checkIfLucky(number))
     cout << "You are lucky today!!!\n";
-  cout << "Sorry, another time.. :(\n";
+  else
+    cout << "Sorry, another time.. :(\n";
 
 
   return 0;
