62bf98a0d9c8e76872f449f1430017b496c31d30
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-10-28 17:54:14 +0200

    depozit function refactor

--- Depozit/src/main.cpp
+++ Depozit/src/main.cpp
@@ -2,25 +2,21 @@
 
 using namespace std;
 
-double day_counter(double begin_sum, double desired_sum) // we'll take 365 days in year and 30 in month
+double day_counter(double begin_sum, double desired_sum, double depo_percent)
 {
-  double days = 0;
-  double depo_percent = 0.1;
-
-  days = log10(desired_sum / begin_sum) / log10(1 + depo_percent);
-
-  return days;
+  return log(desired_sum / begin_sum) / log1p(depo_percent);
 }
 
 
 int main()
 {
   double begin_sum, desired_sum = 0;
+  double depo_percent = 0.1;
 
-  cout << "Enter initial sum and desired sum: \n";
+  cout << "Enter initial sum and desired sum and: \n";
   cin >> begin_sum >> desired_sum;
 
-  int days = day_counter(begin_sum, desired_sum);
+  int days = day_counter(begin_sum, desired_sum, depo_percent);
 
   cout << "Desired sum you will get at " << days << " days.\n";
 
