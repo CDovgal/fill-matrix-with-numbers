1276220d887ded03fb2abf8df4bbc0e5d3f84674
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-11-05 17:52:43 +0200

    Labyrinth(Interface???)

--- Labyrinth/include/Labyrinth.h
+++ Labyrinth/include/Labyrinth.h
@@ -1,41 +1,36 @@
+#pragma once 
+#include <vector>
 
-struct Border
-{
-  bool closed;
-  bool can_be_pulled_up;
-  bool is_enter;
-  bool is_exit;
-};
+using namespace std;
 
-struct Wall
-{
-  bool west_wall_closed;
-  bool east_wall_closed;
-  bool north_wall_closed;
-  bool south_wall_closed;
-};
+typedef vector<vector<int>> Matrix;
 
-struct Backtrack
-{
-  bool goes_back_west;
-  bool goes_back_east;
-  bool goes_back_north;
-  bool goes_back_south;
-};
 
-struct Solution
+struct Cell
 {
-  bool goes_west;
-  bool goes_east;
-  bool goes_north;
-  bool goes_south;
+  int x;
+  int y;
 };
 
 class Labyrinth
 {
 public:
-  Labyrinth();
-  ~Labyrinth();
+  Labyrinth(){};
+  ~Labyrinth(){};
+  void FillMatrix();
+  void PrintMatrix();
+  void FindSolution();
+  Matrix maze;
+  bool IsCellVisited(const Cell& cell);
+  bool IsWall(const Cell& cell);
+  bool IsBorder(const Cell& cell);
+  bool CanBePulledUp(const Cell& cell);
+  void SetEntrance(const Cell& cell);
+  void SetExit(const Cell& cell);
+  void WriteBackTrack(const Cell& cell); // do we need this?
+  void SetCurrentCell(const Cell& cell);
+  Cell entrance() const;
+  Cell exit() const;
 private:
-
+  
 };
--- Labyrinth/src/main.cpp
+++ Labyrinth/src/main.cpp
@@ -6,39 +6,56 @@
 
 using namespace std;
 
-typedef vector <vector<string>> Matrix;
 
-
-void FillMatr()
+bool Labyrinth::IsBorder(const Cell& cell)
 {
-  int rows = 20;
-  int cols = 20;
-  srand(time(0));
-  int enter_point = rand() % rows;
-  int out_point = rand() % cols;
-  vector<vector<int>> maze(rows, vector<int>(cols));
-  for (int i = 0; i < rows; ++i)
-  {
-    for (int j = 0; j < cols; ++j)
-    {
-      maze[0][j];// = border.closed;
-      maze[j][0];// = border.closed;
-      maze[rows - 1][i];// = border.closed;
-      maze[j][cols - 1];// = border.closed;
-      maze[0][rand()];// = Border::is_enter(true);
+  return true;
+}
 
+bool Labyrinth::IsWall(const Cell& cell)
+{
+  return true;
+}
+
+void Labyrinth::SetEntrance(const Cell& cell)
+{
+
+}
+
+void Labyrinth::SetExit(const Cell& cell)
+{
+
+}
+
+void Labyrinth::FillMatrix()
+{
+  int size = 20;
+
+  srand((unsigned)time(0));
+
+  
+  Matrix maze(size);
+
+  for (int i = 0; i < size; ++i)
+  {
+    for (int j = 0; j < size; ++j)
+    {
+      cout << "1";
     }
     cout << endl;
-    
   }
+}
+
+void Labyrinth::FindSolution()
+{
+
 }
 
 
 
 int main()
 {
-  FillMatr();
-  //PathSearch();
-  //OutMatr();
+  Labyrinth lab;
+  lab.FillMatrix();
   return 0;
 }
