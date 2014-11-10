1c42eede1cc31d91e5707b4aec2c5cacb68843df
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-11-10 12:08:25 +0200

    some addings. Have a few questions, for example: - find all neighbors of CurrentCell with all walls intact it means that we need to find neighbour which has neighbours with all walls intact???

--- Labyrinth/src/Wall.cpp
+++ Labyrinth/src/Wall.cpp
@@ -11,6 +11,26 @@ bool Wall::destroyable() const
   return m_is_destroyable;
 }
 
+bool Wall::entrance() const
+{
+  return m_is_entrance;
+}
+
+bool Wall::exit() const
+{
+  return m_is_exit;
+}
+
+void Wall::setEntrance() 
+{
+  m_is_entrance = true;
+}
+
+void Wall::setExit()
+{
+  m_is_exit = true;
+}
+
 void Wall::destroy() // destroy a wall - make a way
 {
   m_is_closed = false;
--- Labyrinth/src/main.cpp
+++ Labyrinth/src/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main()
 {
-  Labyrinth lab(25, 25);
+  Labyrinth lab(10, 10);
   lab.generateMaze();
   lab.PrintMaze();
   return 0;
