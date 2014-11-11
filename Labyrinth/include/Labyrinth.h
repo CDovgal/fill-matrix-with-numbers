f1ffa228f3e7b0b7c058494e84071d456ac0009b
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-11-11 13:24:43 +0200

    commit for check

--- Labyrinth/include/Labyrinth.h
+++ Labyrinth/include/Labyrinth.h
@@ -26,7 +26,6 @@ public:
 
   bool isWall(const Cell& cell) const;
   bool isBorder(const Cell& cell) const;  
-  bool canBeDestroy(const Cell& cell) const;
   
   void setEntrance(const Cell& cell);
   void setExit(const Cell& cell);
@@ -35,9 +34,6 @@ public:
 
   std::vector<Cell> findNeighbours(const Cell& cur_cell);
   bool checkNeighbour(int x, int y);
-  /*Cell checkRight(int x, int y);
-  Cell checkUp(int x, int y);
-  Cell checkDown(int x, int y);*/
 
   Cell entrance() const;
   Cell exit() const;
