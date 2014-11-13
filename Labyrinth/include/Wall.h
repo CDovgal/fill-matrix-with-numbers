37a4b8cec8888607dcd2e986d9c9af0703515f36
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-11-13 17:56:03 +0200

    recommit

--- Labyrinth/include/Wall.h
+++ Labyrinth/include/Wall.h
@@ -4,7 +4,10 @@ class Wall
 {
 public:
   Wall() // closed way, can be destroy by default
-    : m_is_closed(true), m_is_destroyable(true), m_is_entrance(false), m_is_exit(false)
+    : m_is_closed(true), m_is_destroyable(true), 
+    m_up_is_closed(true), m_down_is_closed(true), 
+    m_left_is_closed(true), m_right_is_closed(true), 
+    m_is_entrance(false), m_is_exit(false)
   {}
 
   bool closed() const;
@@ -13,6 +16,17 @@ public:
   bool exit() const;
 
   void destroy(); // destroy a wall - make a way
+  //-------------------
+  bool upWallClosed() const;
+  bool downWallClosed() const;
+  bool leftWallClosed() const;
+  bool rightWallClosed() const;
+
+  void destroyUpWall();
+  void destroyDownWall();
+  void destroyLeftWall();
+  void destroyRightWall();
+  //----------------------
   void restore();
 
   void makeBorder();
@@ -21,6 +35,11 @@ public:
   void setExit();
 
 private:
+  bool m_up_is_closed;
+  bool m_down_is_closed;
+  bool m_left_is_closed;
+  bool m_right_is_closed;
+  //-------------
   bool m_is_closed;
   bool m_is_destroyable;
   bool m_is_entrance;
