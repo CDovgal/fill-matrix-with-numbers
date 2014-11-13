f4252164a67d56fba35d5a751fa8fab62c64ba8d
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-11-13 17:56:03 +0200

    recommit

--- Labyrinth/src/Wall.cpp
+++ Labyrinth/src/Wall.cpp
@@ -35,7 +35,44 @@ void Wall::destroy() // destroy a wall - make a way
 {
   m_is_closed = false;
 }
+//--------------------------------
+bool Wall::upWallClosed() const 
+{
+  return m_up_is_closed;
+}
 
+bool Wall::downWallClosed() const
+{
+  return m_down_is_closed;
+}
+
+bool Wall::leftWallClosed() const
+{
+  return m_left_is_closed;
+}
+
+bool Wall::rightWallClosed() const
+{
+  return m_right_is_closed;
+}
+
+void Wall::destroyUpWall()
+{
+  m_up_is_closed = false;
+}
+void Wall::destroyDownWall()
+{
+  m_down_is_closed = false;
+}
+void Wall::destroyLeftWall()
+{
+  m_left_is_closed = false;
+}
+void Wall::destroyRightWall()
+{
+  m_right_is_closed = false;
+}
+//--------------------------------
 void Wall::restore()
 {
   m_is_closed = true;
