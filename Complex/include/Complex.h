7da210f711251620686d71f7b1b9a9e223840651
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-12-03 17:51:11 +0200

    added copy constructor, copy assignment operator, move constructor, move assignment operator.

--- Complex/include/Complex.h
+++ Complex/include/Complex.h
@@ -1,20 +1,37 @@
 #include <iostream>
+#include <iostream>
 
 class Complex
 {
   double m_real;
   double m_imag;
+  
 public:
-  double Real() const;
-  double Imag() const;
+  Complex();
   Complex(double i_r, double i_i);
+  Complex(const Complex& rhv);
+  Complex(Complex&& rhv);
+  ~Complex(){ std::cout << "COMPLEX DESTRUCTOR" << std::endl; };
+
+  Complex& operator=(Complex&& rhv);
+  Complex& operator=(const Complex& rhv);
+
   Complex operator+(const Complex& i_complex);
   Complex operator*(const Complex& i_complex);
+  Complex operator-(const Complex& i_complex);
+  Complex operator/(const Complex& i_complex);
+
   Complex& operator++();
   Complex& operator--();
 
   Complex operator++(int);
   Complex operator--(int);
 
+  double Real() const;
+  double Imag() const;
+
+  static Complex Substract(const Complex& lhv, const Complex& rhv);
+
   friend std::ostream& operator<<(std::ostream& output, const Complex& i_complex);
+  friend Complex operator-(const Complex& lhv, const Complex& rhv);
 };
--- Complex/src/Complex.cpp
+++ Complex/src/Complex.cpp
@@ -1,9 +1,45 @@
 #include "Complex.h"
 
+Complex::Complex()
+{
+  std::cout << "DEFAULT CONSTRUCTOR" << std::endl;
+}
 
 Complex::Complex(double i_r, double i_i) : m_real(i_r), m_imag(i_i)
 {
+  std::cout << "Overloaded CONSTRUCTOR" << std::endl;
+}
 
+Complex::Complex(const Complex& rhv) : m_real(rhv.m_real), m_imag(rhv.m_imag)
+{
+  std::cout << "COPY CONSTRUCTOR" << std::endl;
+}
+
+Complex::Complex(Complex&& rhv)
+{
+  m_real = rhv.m_real;
+  m_imag = rhv.m_imag;
+  rhv.m_real = 0;
+  rhv.m_imag = 0;
+  std::cout << "MOVE CONSTRUCTOR" << std::endl;
+}
+
+Complex& Complex::operator=(Complex&& rhv)
+{
+  std::cout << "MOVE ASSIGNMENT" << std::endl;
+  /*std::swap(m_real, rhv.m_real);
+  std::swap(m_imag, rhv.m_imag);*/
+  m_real = std::move(rhv.m_real);
+  m_imag = std::move(rhv.m_imag);
+  return *this; 
+}
+
+Complex& Complex::operator=(const Complex& rhv)
+{
+  std::cout << "COPY ASSIGNMENT" << std::endl;
+  m_real = rhv.m_real;
+  m_imag = rhv.m_imag;
+  return *this;
 }
 
 Complex Complex::operator+(const Complex& i_complex)
@@ -14,11 +50,33 @@ Complex Complex::operator+(const Complex& i_complex)
   return result;
 }
 
+Complex Complex::operator-(const Complex& i_complex)
+{
+  Complex result(m_real, m_imag);
+  result.m_real -= i_complex.m_real;
+  result.m_imag -= i_complex.m_imag;
+  return result;
+}
+
+Complex operator-(const Complex& lhv, const Complex& rhv)
+{
+  Complex result(lhv.m_real-rhv.m_real,lhv.m_imag - rhv.m_imag);
+  return result;
+}
+
 Complex Complex::operator*(const Complex& i_complex)
 {
   Complex result(m_real, m_imag);
   result.m_real *= i_complex.m_real;
   result.m_imag *= i_complex.m_imag;
+  return result;
+}
+
+Complex Complex::operator/(const Complex& i_complex)
+{
+  Complex result(m_real, m_imag);
+  result.m_real /= i_complex.m_real;
+  result.m_imag /= i_complex.m_imag;
   return result;
 }
 
@@ -56,12 +114,18 @@ std::ostream& operator<<(std::ostream& output, const Complex& i_complex)
 }
 
 
-double Complex::Real() const 
+double Complex::Real() const
 {
   return m_real;
 }
 
-double Complex::Imag() const 
+double Complex::Imag() const
 {
   return m_imag;
 }
+
+Complex Complex::Substract(const Complex& lhv, const Complex& rhv)
+{
+  return lhv - rhv;
+}
+
