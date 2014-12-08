143c43638576574a1fddc50c36eb595e91ac043b
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-12-08 18:24:04 +0200

    Added

--- GraphicRed/include/Interface.h
+++ GraphicRed/include/Interface.h
@@ -1,11 +1,25 @@
 #include <iostream>
 
+
+class Point
+{
+public:
+  double x;
+  double y;
+};
+
+class Square;
+class Rectangle;
+class Triangle;
+class Circle;
+
 class GraphInterface
 {
 public:
   virtual ~GraphInterface(){};
   virtual void Draw() = 0;
   virtual double getArea() = 0;
+
 };
 
 class Shape : public GraphInterface
@@ -13,24 +27,56 @@ class Shape : public GraphInterface
 public:
   Shape(){};
   ~Shape(){};
+  virtual void Intersect(const Shape& otherObject) = 0;
+
   virtual void Draw(){ std::cout << "Shape draw\n"; };
   virtual double getArea(){ return 2.34; };
 };
 
-class Rectangle :public Shape
+class Polygon : public Shape
+{
+  int m_pointCount;
+public:
+  Polygon(){}
+  virtual void Intersect(const Shape& otherObject);
+  virtual void Intersect(const Square& otherObject);
+  virtual void Intersect(const Rectangle& otherObject);
+  virtual void Intersect(const Triangle& otherObject);
+  virtual void Intersect(const Circle& otherObject);
+  virtual void Draw(){ std::cout << "Polygon draw\n"; }
+  virtual double getArea(){ return 3.00; }
+};
+
+class Square : public Shape
+{
+public:
+  Square(){}
+  ~Square(){}
+  void Draw(){ std::cout << "Square draw\n"; }
+  double getArea(){}
+};
+
+class Rectangle : public Shape
 {
 public:
   Rectangle(){};
   ~Rectangle(){};
-  void Draw(){ std::cout << "Rectangle draw\n"; };
-  double getArea();
+  void Draw(){ std::cout << "Rectangle draw\n"; }
+  double getArea(){ return ; }
 };
 
 class Triangle : public Shape
 {
+  double m_sideA;
+  double m_sideB;
+  double m_sideC;
 public:
-  Triangle(){};
+  Triangle(double i_A, double i_B, double i_C);
   ~Triangle(){};
   void Draw(){ std::cout << "Triangle draw\n"; };
   double getArea();
 };
+
+
+
+
--- Stroustrup/src/main.cpp
+++ Stroustrup/src/main.cpp
@@ -1,6 +1,47 @@
 
 #include <iostream>
 #include <string>
+#include <limits>
+
+int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = 3;
+
+enum En
+{
+  a,
+  b,
+  c,
+  d,
+  e
+};
+
+enum En2
+{
+  
+};
+
+enum En3
+{
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  ten,
+  eleven,
+  twelwe,
+  thirteen,
+  fourteen,
+  fifteen,
+  sixteen,
+  seventeen,
+  eighteen,
+  nineteen,
+  twenty
+};
 
 int main()
 {
@@ -18,6 +59,56 @@ int main()
     const char* season[] = { "dsada", "sdadsa" };
 
     std::cout << ch << " " << s << " " << std::endl;
+
+    std::cout << "\nInt: " << sizeof(int) 
+      << "\nShort: " << sizeof(short int) 
+      << "\nLong: " << sizeof(long) 
+      << "\nDouble: " << sizeof(double) 
+      << "\nChar: " << sizeof(char) 
+      << "\nString: " << sizeof(std::string)
+      << "\nString s: " << sizeof(s)
+      << "\nFloat: " << sizeof(float)
+      << "\nBool: " << sizeof(bool)
+      << "\nChar*: " << sizeof(char*)
+      << "\nInt*: " << sizeof(int*)
+      << "\nString*: " << sizeof(std::string*)
+      << "\nEnum: " << sizeof(En)
+      << "\nEnum2: " << sizeof(En2)
+      << "\nEnum3: " << sizeof(En3)
+
+      << "\n";
+
+    for (char i = 97; i < 123; ++i)
+    {
+      std::cout << i << " ";
+    }
+    std::cout << "\n";
+
+    for (char i = 48; i < 58; ++i)
+    {
+      std::cout << i << " ";
+    }
+    std::cout << "\n";
+
+    std::cout << "Int min: " << std::numeric_limits<int>::min() <<
+      "\n" << "Int max: " << std::numeric_limits<int>::max() <<
+      "\n" << "Char min: " << std::numeric_limits<char>::min() <<
+      "\n" << "Char max: " << std::numeric_limits<char>::max() <<
+      "\n" << "Short min: " << std::numeric_limits<short>::min() <<
+      "\n" << "Short max: " << std::numeric_limits<short>::max() <<
+      "\n" << "Long min: " << std::numeric_limits<long>::min() <<
+      "\n" << "Long max: " << std::numeric_limits<long>::max() <<
+      "\n" << "Float min: " << std::numeric_limits<float>::min() <<
+      "\n" << "Float max: " << std::numeric_limits<float>::max() <<
+      "\n" << "Double min: " << std::numeric_limits<double>::min() <<
+      "\n" << "Double max: " << std::numeric_limits<double>::max() <<
+      "\n" << "Long double min: " << std::numeric_limits<long double>::min() <<
+      "\n" << "Long double max: " << std::numeric_limits<long double>::max() <<
+      "\n" << "Unsigned min: " << std::numeric_limits<unsigned>::min() <<
+      "\n" << "Unsigned max: " << std::numeric_limits<unsigned>::max() << "\n";
+
+    
+
 
     return 0;
 }
