178b56c9ce87df84a08ca16239a119179ed8e747
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-12-15 15:53:35 +0200

    101

--- GraphicRed/include/Interface.h
+++ GraphicRed/include/Interface.h
@@ -1,25 +1,12 @@
 #include <iostream>
 
-
-class Point
-{
-public:
-  double x;
-  double y;
-};
-
-class Square;
-class Rectangle;
-class Triangle;
-class Circle;
-
 class GraphInterface
 {
 public:
   virtual ~GraphInterface(){};
   virtual void Draw() = 0;
-  virtual double getArea() = 0;
-
+  virtual void Input() = 0;
+  virtual void Output() = 0;
 };
 
 class Shape : public GraphInterface
@@ -27,56 +14,46 @@ class Shape : public GraphInterface
 public:
   Shape(){};
   ~Shape(){};
-  virtual void Intersect(const Shape& otherObject) = 0;
-
   virtual void Draw(){ std::cout << "Shape draw\n"; };
-  virtual double getArea(){ return 2.34; };
+  virtual void Input() { std::cout << "Shape input\n"; }
+  virtual void Output(){ std::cout << "Shape output\n"; }
 };
 
-class Polygon : public Shape
+class Polyline : public Shape
 {
   int m_pointCount;
+  Polyline* line;
+  Polyline* curve;
 public:
-  Polygon(){}
-  virtual void Intersect(const Shape& otherObject);
-  virtual void Intersect(const Square& otherObject);
-  virtual void Intersect(const Rectangle& otherObject);
-  virtual void Intersect(const Triangle& otherObject);
-  virtual void Intersect(const Circle& otherObject);
-  virtual void Draw(){ std::cout << "Polygon draw\n"; }
+  Polyline(){}
+  Polyline* Intersect(const Polyline& otherObject);
+  virtual void Draw(){ std::cout << "Polyline draw\n"; }
   virtual double getArea(){ return 3.00; }
 };
 
-class Square : public Shape
-{
-public:
-  Square(){}
-  ~Square(){}
-  void Draw(){ std::cout << "Square draw\n"; }
-  double getArea(){}
-};
-
-class Rectangle : public Shape
-{
-public:
-  Rectangle(){};
-  ~Rectangle(){};
-  void Draw(){ std::cout << "Rectangle draw\n"; }
-  double getArea(){ return ; }
-};
-
-class Triangle : public Shape
+class Triangle : public Polyline
 {
   double m_sideA;
   double m_sideB;
   double m_sideC;
 public:
-  Triangle(double i_A, double i_B, double i_C);
+  Triangle(Polyline* i_A, Polyline* i_B, Polyline* i_C) : ;
   ~Triangle(){};
   void Draw(){ std::cout << "Triangle draw\n"; };
   double getArea();
 };
 
+class Ellipse : public Polyline
+{
+  double m_sideA;
+  double m_sideB;
+  double m_sideC;
+public:
+  Ellipse(double i_A, double i_B, double i_C);
+  ~Ellipse(){};
+  void Draw(){ std::cout << "Ellipse draw\n"; };
+  double getArea();
+};
 
 
 
