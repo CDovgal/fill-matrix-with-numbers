7259bf4c04ea0f8a7c9a423d5543ebf2103324ed
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-12-18 17:40:35 +0200

    Fix

--- GraphicRed/src/Interface.cpp
+++ GraphicRed/src/Interface.cpp
@@ -1,14 +1,58 @@
 #include "Interface.h"
 #include <iostream>
+#define PI 3.14159265
 
-Triangle::Triangle(double i_A, double i_B, double i_C) : m_sideA(i_A), m_sideB(i_B), m_sideC(i_C)
+Polyline::Polyline(const Point& i_a, const Point& i_b)
+{
+  std::cout << "Polyline constr\n";
+}
+
+Triangle::Triangle(const Point& i_A, const Point& i_B, const Point& i_C) : Polyline(i_A, i_B), m_a(i_A), m_b(i_B), m_c(i_C)
 {
   std::cout << "Triangle constr\n";
 }
 
+Ellipse::Ellipse(const Point& i_center, const Point& i_a, const Point& i_b) : Polyline(i_a, i_b), m_center(i_center), m_a(i_a), m_b(i_b)
+{
+  std::cout << "Ellipse constr\n";
+}
+
 double Triangle::getArea()
 {
-  double p = (m_sideA + m_sideB + m_sideC) / 2;
-  double s = std::sqrt(p*(p-m_sideA)*(p-m_sideB)*(p-m_sideC));
+  /*double p = (m_a + m_b + m_c) / 2;
+  double s = std::sqrt(p*(p-m_sideA)*(p-m_sideB)*(p-m_sideC));*/
+  double s = abs((m_a.m_x*(m_b.m_y - m_c.m_y)+ m_b.m_x*(m_c.m_y-m_a.m_y)+m_c.m_x*(m_a.m_y-m_b.m_y))/2);
   return s;
 }
+
+double Ellipse::getArea()
+{
+  double s = 5;
+  return s;
+}
+
+
+Polyline* Triangle::Intersect(Triangle* i_tr)
+{
+  std::cout << "Triangle intersects with triangle\n";
+  return IntersectTriangleWithTriangle(this, i_tr);
+}
+
+Polyline* Triangle::Intersect(Ellipse* i_ell)
+{
+  std::cout << "Triangle intersects with ellipse\n";
+  return IntersectTriangleWithEllipse(this, i_ell);
+};
+
+
+Polyline* Ellipse::Intersect(Triangle* i_tr)
+{
+  std::cout << "Ellipse intersects with triangle\n";
+  return IntersectTriangleWithEllipse(i_tr, this);
+};
+
+Polyline* Ellipse::Intersect(Ellipse* i_ell)
+{
+  std::cout << "Ellipse intersects with ellipse\n";
+  return IntersectEllipseWithEllipse(this, i_ell);
+};
--- GraphicRed/src/main.cpp
+++ GraphicRed/src/main.cpp
@@ -4,11 +4,15 @@
 
 int main()
 {
-  //GraphInterface *graph = new Shape;
+  
+  Polyline* tr = new Triangle({2,3},{3,4},{4,5});// tri(2, 3, 4);
+  Polyline* ell = new Ellipse({ 2, 3 }, { 3, 4 }, { 4, 5 });// ell;
+  tr->Draw();
+  //tr->Intersect(ell);
+  //ell->Intersect(tr);
+  std::cout << tr->getArea() << std::endl;
+  std::cout << ell->getArea() << std::endl;
 
-  Triangle tri(2, 3, 4);
-  tri.Draw();
-  std::cout << tri.getArea() << std::endl;
 
   return 0;
 }
