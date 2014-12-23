cda0a19f2ff1544465133edd072102e9fa071cda
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-12-23 16:59:47 +0200

    107

--- GraphicRed/src/main.cpp
+++ GraphicRed/src/main.cpp
@@ -4,15 +4,24 @@
 
 int main()
 {
-  
-  Polyline* tr = new Triangle({2,3},{3,4},{4,5});// tri(2, 3, 4);
-  Polyline* ell = new Ellipse({ 2, 3 }, { 3, 4 }, { 4, 5 });// ell;
-  tr->Draw();
-  //tr->Intersect(ell);
-  //ell->Intersect(tr);
-  std::cout << tr->getArea() << std::endl;
-  std::cout << ell->getArea() << std::endl;
-
+  {
+    Polyline* tr = new Triangle({ 2, 3 }, { 3, 4 }, { 4, 5 });// tri(2, 3, 4);
+    Polyline* ell = new Ellipse({ 2, 3 }, { 3, 4 }, { 4, 5 });// ell;
+    std::vector<Segment> seg_list;
+    Polyline* pl = new Polyline(seg_list);
+    pl->Intersect(tr->Intersect(ell));
+    tr->Draw();
+    tr->Intersect(ell);
+    //ell->Intersect(tr);
+    //tr->Intersect(ell);
+    //ell->Intersect(tr);
+    std::cout << tr->getArea() << std::endl;
+    std::cout << ell->getArea() << std::endl;
+    delete tr;
+    delete ell;
+  }
+  if (_CrtDumpMemoryLeaks())
+    std::cout << "OLOLO!!!!! Leaks.... :(\n";
 
   return 0;
 }
