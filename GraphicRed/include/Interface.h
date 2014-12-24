0b18e44360ae12754fa86b4cd79649f21e229a86
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2014-12-24 17:38:01 +0200

    109

--- GraphicRed/include/Interface.h
+++ GraphicRed/include/Interface.h
@@ -2,6 +2,18 @@
 #include <vector>
 #include <limits.h>
 
+
+class not_implemented : public std::exception
+{
+  std::string m_msg;
+public:
+  not_implemented(const std::string& msg) : m_msg(msg){}
+  const char* what() const throw()
+  {
+    return m_msg.c_str();
+  }
+};
+
 class Point
 {
   double m_x;
