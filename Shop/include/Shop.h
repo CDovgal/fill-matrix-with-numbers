eddf90064c9dead445ceca6d363a090a37533e50
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2015-01-20 11:56:00 +0200

    refactored Shop

--- Shop/include/Shop.h
+++ Shop/include/Shop.h
@@ -1,44 +1,23 @@
 #pragma once
 #include <iostream>
-#include "BaseFactory.h"
-#include "Category.h"
 #include <map>
 #include <string>
 #include <vector>
 
 class Shop
 {
-  Shop();
-  ~Shop();
   Shop(const Shop&) = delete;
   Shop& operator=(const Shop&) = delete;
   void load_db();
   int m_startup_sum;
-  std::vector<Product*> m_prod_vec;
-  void clear_data();
-  Product* get_product(std::istream& is);
 public:
-  static Shop& instance()
-  {
-    static Shop instance;
-    return instance;
-  }
-  void Generate();
-  void Input();
-  void Output();
+  Shop();
+  ~Shop();
   void set_startup_sun(int i_sum);
   int start_menu();
   int admin_menu();
 };
 
-
-typedef Product* (*PF)();
-
-class IoRegistryMap
-{
-public:
-  void RegisterClasses();
-};
 
 
 
--- Shop/src/Shop.cpp
+++ Shop/src/Shop.cpp
@@ -2,26 +2,9 @@
 #include <fstream>
 #include "Shop.h"
 
-
-std::map<std::string, PF> io_map;
-
-void IoRegistryMap::RegisterClasses()
-{
-  io_map["class Alive"] = &Alive::new_alive;
-  io_map["class Unalive"] = &Unalive::new_unalive;
-}
-
 void Shop::load_db()
 {
   std::cout << "Database loaded..." << std::endl;
-}
-
-void Shop::clear_data()
-{
-  if (!m_prod_vec.empty())
-  for (unsigned i = 0; i < m_prod_vec.size(); ++i)
-    delete m_prod_vec.at(i);
-  m_prod_vec.clear();
 }
 
 Shop::Shop()
@@ -31,79 +14,6 @@ Shop::Shop()
 
 Shop::~Shop()
 {
-  clear_data();
-}
-
-void Shop::Generate()
-{
-  Alive *al = new Alive(Good("Rose", 300));
-  Alive *al2 = new Alive(Good("Astra", 200));
-  Alive *al3 = new Alive(Good("Gvozdika", 500));
-  Alive *al4 = new Alive(Good("Liliya", 100));
-  m_prod_vec.push_back(al);
-  m_prod_vec.push_back(al2);
-  m_prod_vec.push_back(al3);
-  m_prod_vec.push_back(al4);
-
-  Unalive *unal = new Unalive(Good("Seed", 300));
-  Unalive *unal2 = new Unalive(Good("Gorshok", 200));
-  Unalive *unal3 = new Unalive(Good("udobrenie", 500));
-  Unalive *unal4 = new Unalive(Good("Ololo", 100));
-  m_prod_vec.push_back(unal);
-  m_prod_vec.push_back(unal2);
-  m_prod_vec.push_back(unal3);
-  m_prod_vec.push_back(unal4);
-}
-
-Product* Shop::get_product(std::istream& is)
-{
-  std::string name;
-  std::getline(is, name);
-  PF f = io_map[name];
-  if (f == 0) return nullptr;
-  return f();
-}
-
-void Shop::Input()
-{
-  clear_data();
-  Product *product = nullptr;
-  size_t product_count;
-  std::ifstream input("Products.dat");
-  if (input.is_open())
-  {
-    input >> product_count;
-    for (unsigned i = 0; i < product_count;)
-    {
-      product = get_product(input);
-      if (product != nullptr)
-      {
-        product->Input(input);
-        m_prod_vec.push_back(product);
-        ++i;
-      }
-    }
-  }
-  else
-    std::cout << "Unable to open file\n";
-  input.close();
-}
-
-void Shop::Output()
-{
-  std::ofstream aliveoutput("Products.dat");
-  if (aliveoutput.is_open())
-  {
-    aliveoutput << m_prod_vec.size() << std::endl;
-    for (unsigned i = 0; i < m_prod_vec.size(); ++i)
-    {
-      aliveoutput << typeid(*m_prod_vec.at(i)).name() << std::endl;
-      m_prod_vec.at(i)->Output(aliveoutput);
-    }
-  }
-  else
-    std::cout << "Unable to open file\n";
-  aliveoutput.close();
 }
 
 void Shop::set_startup_sun(int i_sum)
@@ -155,8 +65,7 @@ int Shop::admin_menu()
     std::cout << m_startup_sum << std::endl;
     break;
   case 2:
-    //Shop::Generate();
-    std::cout << m_prod_vec.size() << std::endl;
+    //std::cout << m_prod_vec.size() << std::endl;
     break;
   }
 
