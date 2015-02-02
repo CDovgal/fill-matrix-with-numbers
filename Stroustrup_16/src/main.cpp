25070c9a25269433a4b31f09c54589134fbdb3a4
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2015-02-02 17:39:44 +0200

    170

--- Stroustrup_16/src/main.cpp
+++ Stroustrup_16/src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iterator>
 
 
 int main()
@@ -23,20 +24,59 @@ int main()
   std::cout << std::endl;
 
   std::vector<std::string> fruits;
-  std::string fruit;
+  /*std::string fruit;
   for (int i = 0; i < 6; ++i)
   {
-    std::cin >> fruit;
-    fruits.push_back(fruit);
+  std::cin >> fruit;
+  fruits.push_back(fruit);
   }
   if (!std::is_sorted(fruits.begin(), fruits.end()))
-    std::sort(fruits.begin(), fruits.end());
+  std::sort(fruits.begin(), fruits.end());
   else
-    std::cout << "The container is sorted." << std::endl;
+  std::cout << "The container is sorted." << std::endl;
   for (unsigned i = 0; i < fruits.size(); ++i)
-    std::cout << fruits.at(i) << " ";
+  std::cout << fruits.at(i) << " ";
   std::cout << std::endl;
-  std::cout << fruits.size() << std::endl;
+  std::cout << fruits.size() << std::endl;*/
+  fruits.push_back("peach");
+  fruits.push_back("apple");
+  fruits.push_back("kivifruit");
+  fruits.push_back("pear");
+  fruits.push_back("starfruit");
+  fruits.push_back("grape");
+  fruits.push_back("avocado");
+  fruits.push_back("orange");
+  fruits.push_back("lemon");
+
+  std::vector<std::string> citrus;
+  citrus.push_back("orange");
+  citrus.push_back("lemon");
+
+  
+
+  std::vector<std::string>::iterator citrus_iter = citrus.begin();
+  for (std::vector<std::string>::iterator fruit_iter = fruits.begin(); fruit_iter != fruits.end(); ++fruit_iter)
+  {
+    
+  }
+
+  /*for (unsigned i = 0; i < fruits.size(); ++i)
+  {
+  if (fruits.at(i).at(0) == 'a')
+  std::cout << fruits.at(i) << std::endl;
+  }*/
+  
+
+  /*for (unsigned i = 0; i < fruits.size(); ++i)
+  {
+    if (fruits.at(i).at(0) == 'a')
+      fruits.erase(fruits.begin() + i);
+  }*/
+  for (unsigned i = 0; i < fruits.size(); ++i)
+  {
+    std::cout << fruits.at(i) << std::endl;
+  }
+
 
 
 
