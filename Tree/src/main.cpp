6ee3fe5f281934e3d5465f7b8af84e44645fd18f
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2015-02-23 18:14:13 +0200

    added inserting and viewing of tree

--- Tree/src/main.cpp
+++ Tree/src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "Tree.h"
 
 int main()
 {
+  Tree<int> tr;
+  tr.insert(5);
+  tr.insert(3);
+  tr.insert(8);
+  tr.insert(6);
+  tr.insert(12);
+  tr.insert(7);
+  tr.insert(10);
+  tr.insert(2);
+  tr.insert(4);
+  tr.insert(21);
+  tr.insert(36);
+  tr.insert(18);
+  tr.insert(13);
+  tr.Show();
   return 0;
 }
