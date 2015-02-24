481e7432eccf4a8219730050852214c14aa97490
Volodymyr.Lysenko <Volodymyr.Lysenko@localhost>
2015-02-24 16:58:07 +0200

    Tree: added witing to file

--- Tree/src/main.cpp
+++ Tree/src/main.cpp
@@ -3,20 +3,32 @@
 
 int main()
 {
-  Tree<int> tr;
-  tr.insert(5);
-  tr.insert(3);
-  tr.insert(8);
-  tr.insert(6);
-  tr.insert(12);
-  tr.insert(7);
-  tr.insert(10);
-  tr.insert(2);
-  tr.insert(4);
-  tr.insert(21);
-  tr.insert(36);
-  tr.insert(18);
-  tr.insert(13);
-  tr.Show();
+  {
+    Tree<int> tr;
+    tr.insert(5);
+    tr.insert(3);
+    tr.insert(8);
+    tr.insert(6);
+    tr.insert(12);
+    tr.insert(7);
+    tr.insert(10);
+    tr.insert(2);
+    tr.insert(4);
+    tr.insert(21);
+    tr.insert(36);
+    tr.insert(18);
+    tr.insert(13);
+    tr.insert(16);
+    tr.insert(17);
+    tr.Show();
+    std::cout << tr.size() << std::endl;
+    std::cout << tr.longest_branch() << std::endl;
+    tr.BFS();
+    tr.save_tree();
+  }
+  if (_CrtDumpMemoryLeaks())
+    std::cout << "MEMORY LEAKS." << std::endl;
+  else
+    std::cout << "MEMORY OK." << std::endl;
   return 0;
 }
